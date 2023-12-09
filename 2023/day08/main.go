6ffaf02ac50cb80b960a877229f254cae12db704
exceliber <fredrik.abrahamsson12@gmail.com>
2023-12-09 09:43:21 +0100

    day08

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/abrahamfredrik/aoc/utils"
+	"github.com/abrahamfredrik/aoc/utils/math"
 )
 
 func main() {
@@ -13,11 +16,72 @@ func main() {
 }
 
 func day1() {
-	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	goalList := pathFinder([]string{"AAA"})
+	fmt.Println(goalList)
 }
 
 func day2() {
+	_, m := parseInput()
+	positionList := []string{}
+	for k := range m {
+		if k[2] == 'A' {
+			positionList = append(positionList, k)
+		}
+	}
+	goalList := pathFinder(positionList)
+
+	fmt.Println("LCM: ", aocMath.LeastCommonMultiple(goalList[0], goalList[1], goalList[2:]...))
+	for _, v := range goalList {
+		fmt.Println(v)
+	}
+}
+
+func pathFinder(startingPoints []string) []int {
+	instructions, m := parseInput()
+	stepsToGoal := make([]int, len(startingPoints))
+	for i, start := range startingPoints {
+		index := 0
+		counter := 0
+
+		for true {
+			inst := instructions[index]
+			if inst == "R" {
+				start = m[start].right
+			} else {
+				start = m[start].left
+			}
+			counter++
+			if start[2] == 'Z' {
+				break
+			}
+
+			if index == len(instructions)-1 {
+				index = 0
+			} else {
+				index++
+			}
+		}
+		stepsToGoal[i] = counter
+	}
+	return stepsToGoal
+}
+
+func parseInput() ([]string, map[string]element) {
 	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	inst := strings.Split(input[0], "")
+	elementRegexp, _ := regexp.Compile("[0-9A-Z]{3}")
+	m := map[string]element{}
+	for _, el := range input[2:] {
+		parts := elementRegexp.FindAllString(el, -1)
+		if len(parts) != 3 {
+			panic("Fail in parsing")
+		}
+		m[parts[0]] = element{parts[1], parts[2]}
+	}
+	return inst, m
+}
+
+type element struct {
+	left  string
+	right string
 }
--- utils/slice.go
+++ utils/slice.go
@@ -12,6 +12,18 @@ func RemoveFromSlice[T string | int | rune](s []T, remove T) []T {
 	panic(fmt.Sprintf("Could not remove %v from slice %v", remove, s))
 }
 
+func RemoveDuplicates[T string | int | rune](s []T) []T {
+	m := map[T]bool{}
+	list := []T{}
+	for _, v := range s {
+		if !m[v] {
+			m[v] = true
+			list = append(list, v)
+		}
+	}
+	return list
+}
+
 func CopySlice[T string | int | rune](s []T) []T {
 	n := make([]T, len(s))
 	for i, v := range s {

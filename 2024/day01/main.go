c460643f7e9916079ea6211d0b622b2439745092
Fredrik Abrahamsson <fredrik.abrahamsson12@gmail.com>
2024-12-02 22:36:58 +0100

    New go version

--- 2024/day01/main.go
+++ 2024/day01/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
+	"strings"
 
 	"github.com/abrahamfredrik/aoc/utils"
 )
@@ -13,11 +16,38 @@ func main() {
 }
 
 func day1() {
-	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	left, right := setupList()
+	sort.Ints(left)
+	sort.Ints(right)
+	sum := 0
+	for i, value := range left {
+		sum += int(math.Abs(float64(value) - float64(right[i])))
+	}
+	fmt.Println(sum)
 }
 
 func day2() {
+	left, right := setupList()
+	sum := 0
+	for _, leftValue := range left {
+		for _, rightValue := range right {
+			if leftValue == rightValue {
+				sum += leftValue
+			}
+		}
+		
+	}
+	fmt.Println(sum)
+}
+
+func setupList() ([]int,[]int) {
 	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	left := make([]int, len(input))
+	right := make([]int, len(input))
+	for i, row := range input {
+		fields := strings.Fields(row)
+		left[i] = utils.ToInt(fields[0])
+		right[i] = utils.ToInt(fields[1])
+	}
+	return left, right
 }

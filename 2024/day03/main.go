e7a2191b4f70b3476de1d469206ec5811538a9ec
Fredrik Abrahamsson <fredrik.abrahamsson12@gmail.com>
2024-12-03 08:10:16 +0100

    day 3

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/abrahamfredrik/aoc/utils"
 )
@@ -12,12 +14,44 @@ func main() {
 	day2()
 }
 
+var mulRegex, _ = regexp.Compile(`mul\(\d*,\d*\)`)
+
 func day1() {
-	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	input := strings.Join(utils.ReadInput("dayInput.txt"), "")
+	countMultiplication(input)
 }
 
 func day2() {
-	input := utils.ReadInput("dayInput.txt")
-	fmt.Println(input)
+	input := strings.Join(utils.ReadInput("dayInput.txt"), "")
+
+	cleanedInput := ""
+	buffer := ""
+	active := true
+	for _, s := range input {
+		buffer = buffer + string(s)
+		if active {
+			cleanedInput = cleanedInput + string(s)
+		}
+
+		if strings.HasSuffix(buffer, "do()") {
+			active = true
+		} else if strings.HasSuffix(buffer, "don't()") {
+			active = false
+		}
+	}
+
+	countMultiplication(cleanedInput)
+}
+
+func countMultiplication(input string) {
+	hits := mulRegex.FindAllString(input, -1)
+	sum := 0
+	for _, hit := range hits {
+		firstRemoved := strings.ReplaceAll(hit, `mul(`, ``)
+		allRemoved := strings.ReplaceAll(firstRemoved, `)`, ``)
+		numbersString := strings.Split(allRemoved, ",")
+		numbers := utils.ListToInts(numbersString)
+		sum += numbers[0] * numbers[1]
+	}
+	fmt.Println(sum)
 }
